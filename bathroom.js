a145a5944857da08820d9d8a34d5634ed5e48f9a
AtharvSaraswat123 <87174532+AtharvSaraswat123@users.noreply.github.com>
2022-06-18 20:32:28 +0530

    Add files via upload

---
+++ bathroom.js
@@ -0,0 +1,21 @@
+status="";
+img="";
+function preload(){
+  img=loadImage('bathroom.jpg.jpg');
+}
+function setup(){
+    canvas=createCanvas(380,380);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+function modelLoaded(){
+        console.log("Model Loaded!");
+        status=true;
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
